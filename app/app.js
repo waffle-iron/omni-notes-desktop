a41981bb7f7f4e30238b1218f1ef5624f973ed17
Federico Iosue <federico.iosue@gmail.com>
2016-03-14 22:58:47 +0100

    Added AngularJS app module

---
+++ app/app.js
@@ -0,0 +1,21 @@
+(function () {
+    'use strict';
+
+    var _templateBase = './scripts';
+
+    angular.module('app', [
+        'ngRoute',
+        'ngMaterial',
+        'ngAnimate'
+    ])
+    .config(['$routeProvider', function ($routeProvider) {
+            $routeProvider.when('/', {
+                templateUrl: _templateBase + '/customer/customer.html' ,
+                controller: 'customerController',
+                controllerAs: '_ctrl'
+            });
+            $routeProvider.otherwise({ redirectTo: '/' });
+        }
+    ]);
+
+})();
