f64775c414d26a1154b8f45bea3c42f5559d3782
Federico Iosue <federico.iosue@gmail.com>
2016-03-14 23:27:09 +0100

    Added application logo

--- main.js
+++ main.js
@@ -12,7 +12,7 @@ let mainWindow;
 
 function createWindow () {
   // Create the browser window.
-  mainWindow = new BrowserWindow({width: 800, height: 600});
+  mainWindow = new BrowserWindow({width: 800, height: 600, icon: __dirname + '/app/assets/images/logo.png',});
 
   // and load the index.html of the app.
   mainWindow.loadURL('file://' + __dirname + '/index.html');
