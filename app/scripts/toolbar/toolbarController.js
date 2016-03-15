2ad6f8fd581bdddeeda23529efa29fc5c2e6c488
Federico Iosue <federico.iosue@gmail.com>
2016-03-15 03:15:09 +0100

    Refactored "drawer" components into "toolbar"

--- app/scripts/toolbar/toolbarController.js
+++ app/scripts/toolbar/toolbarController.js
@@ -1,5 +1,5 @@
 angular.module('app').controller('drawerController', ["$scope", '$q', '$log', '$window', function($scope, $q, $log, $window) {
-  $log.debug('drawer loaded');
+  $log.debug('toolbar loaded');
 
   $scope.exit = function() {
     $window.close();
