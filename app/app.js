bffbb42bd89caf3b325927a83415b221f14181f7
Federico Iosue <federico.iosue@gmail.com>
2016-03-16 23:37:53 +0100

    Globally registered app instance variable

--- app/app.js
+++ app/app.js
@@ -1,17 +1,19 @@
+var app;
+
 (function () {
     'use strict';
 
     var _templateBase = 'app/scripts';
 
-    angular.module('app', [
+    app = angular.module('app', [
         'ngRoute',
         'ngMaterial',
         'ngAnimate'
     ])
     .config(['$routeProvider', function ($routeProvider) {
-            $routeProvider.when('/', {
-                templateUrl: _templateBase + '/list/list.html',
-                controllerAs: '_ctrl'
+            $routeProvider
+            .when('/', {
+                templateUrl: _templateBase + '/list/list.html'
             });
             $routeProvider.otherwise({ redirectTo: '/' });
         }
