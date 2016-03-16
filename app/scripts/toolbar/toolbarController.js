304d2bbaf728c20949de3c4d0458dd7d239d2b39
Federico Iosue <federico.iosue@gmail.com>
2016-03-17 00:30:23 +0100

    Removed window frame and added draggable toobar

--- app/scripts/toolbar/toolbarController.js
+++ app/scripts/toolbar/toolbarController.js
@@ -1,4 +1,4 @@
-angular.module('app').controller('drawerController', ["$scope", '$q', '$log', '$window', function($scope, $q, $log, $window) {
+angular.module('app').controller('toolbarController', ["$scope", '$q', '$log', '$window', function($scope, $q, $log, $window) {
   $log.debug('toolbar loaded');
 
   $scope.exit = function() {
--- main.js
+++ main.js
@@ -13,7 +13,7 @@ function createWindow() {
   mainWindow = new BrowserWindow({
     width: 800,
     height: 600,
-    //frame: false,
+    frame: false,
     icon: __dirname + '/app/assets/images/logo.png'
   });
 
